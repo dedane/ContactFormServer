b70634f3b63d2cdbf6e51a9a4afa37a62540b09d
dedane <evanskeema@gmail.com>
2021-07-12 23:25:29 +0300

    answered

--- config/mailer.js
+++ config/mailer.js
@@ -2,12 +2,16 @@ const nodemailer = require('nodemailer');
 const hbs = require("nodemailer-express-handlebars")
 
 var transporter = nodemailer.createTransport({
-    host: 'gmail.com',
+    host: 'smtp.gmail.com',
     port: 465,
-    secure: true, //ssl
+    secure: true,
     auth: {
+            type: 'OAuth2',
             user:'evanskeema@gmail.com',
-            pass:'findout123'
+            pass:'findout123',
+            clientId :'1003419225201-gub5mihaoe2oi1kg6n9qdemvrb20f302.apps.googleusercontent.com',
+            clientSecret: '5kxMVZ-syAherVlNXLspSpU2',
+            expires: 1484314697598
     },
     tls: {
         rejectUnauthorized: false
