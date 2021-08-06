130be30b3cdf9c888dce3c5971746e6849a379a4
dedane <evanskeema@gmail.com>
2021-08-06 05:54:26 +0300

    choosingme

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-//const mongoose =require('mongoose');
+const mongoose =require('mongoose');
 
 const contacts = require('./routes/contacts');
 require('dotenv').config({ path: './.env'});
